20a76f994160db8de23e2572d7883e79fb2cabf6
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-09-26 23:42:34 -0700

    initial commmit, nothing ready yet

---
+++ content_script.js
@@ -0,0 +1,8 @@
+// Put all the javascript code here, that you want to execute after page load.
+//console.log(document.getElementsByClassName("tagline")[0]);
+divs = document.getElementsByTagName("div");
+console.log(divs);
+console.log(
+  document.getElementsByTagName("div").getElementsByClassName("tagline"),
+);
+console.log("test");
