36a798d0ff001e1a432fce96b40653deadd3d765
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-09-27 02:49:40 -0700

    yeah

--- content_script.js
+++ content_script.js
@@ -1,8 +1,3 @@
 // Put all the javascript code here, that you want to execute after page load.
-//console.log(document.getElementsByClassName("tagline")[0]);
-divs = document.getElementsByTagName("div");
-console.log(divs);
-console.log(
-  document.getElementsByTagName("div").getElementsByClassName("tagline"),
-);
+Console.log(document.getElementsByClassName("hover redditname"));
 console.log("test");
