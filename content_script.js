7539eca8be472282b36a503b9862084b1539a66b
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-10-01 04:23:45 -0700

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,3 +1,3 @@
 // Put all the javascript code here, that you want to execute after page load.
-Console.log(document.getElementsByClassName("hover redditname"));
+console.log(document.getElementsByClassName("hover redditname")[1]);
 console.log("test");
